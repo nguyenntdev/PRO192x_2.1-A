2c76345f7f680ff75fc0bfe6caa9c318aed76708
Nguyễn N. Thiện <kakangocthien109@gmail.com>
2021-09-29 00:17:39 -0700

    JAVa

---
+++ Labs/Lab011/src/com/labs/lab011/Main.java
@@ -0,0 +1,11 @@
+package com.labs.lab011;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.print("//////////////////////\n");
+        System.out.print("|| Victory is mine! ||\n");
+        System.out.print("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n");
+    }
+}
---
+++ Labs/lab011.java
@@ -0,0 +1,9 @@
+package lab011;
+
+public class lab011 {
+    public static void main(String[] args) {
+        System.out.print("//////////////////////");
+        System.out.print("|| Victory is mine! ||");
+        System.out.print("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+    }
+}
