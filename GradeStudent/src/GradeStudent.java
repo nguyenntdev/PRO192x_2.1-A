41a01f61389e38e779227d3418387fac23265258
Nguyễn N. Thiện <kakangocthien109@gmail.com>
2021-10-10 17:12:39 +0700

    Nothing

--- GradeStudent/src/GradeStudent.java
+++ GradeStudent/src/GradeStudent.java
@@ -1,5 +1,125 @@
+import java.util.*;
+import java.text.*;
+
+/* TODO: 
+* - write homework & report method
+* - write array start from 1
+*/
 public class GradeStudent {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    static void clrscr() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+
+    }
+
+    static void sep() {
+        System.out.println("====================================");
+    }
+
+    static void brand() {
+        System.out.println("S  T  U  D  E  N  T   G  R  A  D  E");
+    }
+
+    static void intro() {
+        clrscr();
+        sep();
+        brand();
+        sep();
+        System.out.println("\nThis program reads exam/homework scores and reports your overall course grade.");
+    }
+
+    static float midTerm() {
+        DecimalFormat df = new DecimalFormat("#.#");
+
+        Scanner input = new Scanner(System.in);
+        clrscr();
+        sep();
+        brand();
+        sep();
+        System.out.println("M  I  D   T  E  R  M");
+        sep();
+        System.out.println("Please enter weight score: ");
+        float midTermWeightScore = input.nextFloat();
+        System.out.println("Please enter score earned: ");
+        float midTermScoreEarned = input.nextFloat();
+        System.out.println("Were score shifted [y/n]?");
+        String midTermShiftedChoice = input.next();
+
+        int midTermShiftedAmount = 0;
+        if (midTermShiftedChoice.equalsIgnoreCase("y")) {
+            System.out.println("Please enter shifted amount: ");
+            midTermShiftedAmount = input.nextInt();
+        }
+        sep();
+        float midTermWeightedScore = ((midTermScoreEarned + midTermShiftedAmount) / 100) * midTermWeightScore;
+        System.out.println("Total score: " + (midTermScoreEarned + midTermShiftedAmount));
+        System.out.println("Weighted score: " + df.format(midTermWeightedScore));
+
+        return midTermWeightedScore;
+    }
+
+    static float finalTerm() {
+        DecimalFormat df = new DecimalFormat("#.#");
+
+        Scanner input = new Scanner(System.in);
+
+        sep();
+        System.out.println("F  I  N  A  L   T  E  R  M");
+        sep();
+        System.out.println("Please enter weight score: ");
+        float finalTermWeightScore = input.nextFloat();
+        System.out.println("Please enter score earned: ");
+        float finalTermScoreEarned = input.nextFloat();
+        System.out.println("Were score shifted [y/n]?");
+        String finalTermShiftedChoice = input.next();
+        
+        float finalTermShiftedAmount = 0;
+        if (finalTermShiftedChoice.equalsIgnoreCase("y")) {
+            System.out.println("Please enter shifted amount: ");
+            finalTermShiftedAmount = input.nextInt();
+        }
+        sep();
+        float finalTermTotalScore = (finalTermShiftedAmount + finalTermScoreEarned);
+        if (finalTermTotalScore > 100) {
+            finalTermTotalScore = 100;
+        }
+        float finalTermWeightedScore = (finalTermTotalScore / 100) * finalTermWeightScore ;
+        System.out.println("Total score: " + finalTermTotalScore);
+        System.out.println("Weighted score: " + finalTermWeightedScore);
+
+        return finalTermWeightedScore;
+    }
+
+    static float homework() {
+        DecimalFormat df = new DecimalFormat("#.#");
+
+        Scanner input = new Scanner(System.in);
+
+        sep();
+        System.out.println("H  O  M  E  W  O  R  K");
+        sep();
+        System.out.println("Please enter weight score: ");
+        float homeworkWeightScore = input.nextFloat();
+        System.out.println("Please enter number of homework: ");
+        int n = input.nextInt();
+        float[] scores = new float[n];
+        float[] max = new float[n];
+        for (int i = 0; i < n; i++) {
+            System.out.println("Please enter the Assignment " + i + " score:");
+            scores[i] = input.nextFloat();
+            
+            System.out.println("Please enter the Assignment " + i + " max score:");
+            max[i] = input.nextFloat();
+        }
+
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+        intro();
+        float midTermWeightedScore = midTerm();
+        float finalTermWeightedScore = finalTerm();
+        homework();
     }
 }
