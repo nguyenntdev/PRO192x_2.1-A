e7c6edb96e1650eaae425789539277b63cf2adfb
Nguyễn N. Thiện <kakangocthien109@gmail.com>
2021-10-11 14:44:11 +0700

    .

--- GradeStudent/src/GradeStudent.java
+++ GradeStudent/src/GradeStudent.java
@@ -51,8 +51,13 @@ public class GradeStudent {
             midTermShiftedAmount = input.nextInt();
         }
         sep();
-        float midTermWeightedScore = (((float)midTermScoreEarned + midTermShiftedAmount) / 100) * midTermWeightScore;
-        System.out.println("Total points: " + (midTermScoreEarned + midTermShiftedAmount) + "/100");
+        float midTermWeightedScore = (((float) midTermScoreEarned + midTermShiftedAmount) / 100) * midTermWeightScore;
+        System.out.println("Total points: "
+                + ((midTermScoreEarned + midTermShiftedAmount > 100) ? 100 : midTermScoreEarned + midTermShiftedAmount)
+                + "/100");
+        if (midTermWeightedScore > midTermWeightScore) {
+            midTermWeightedScore = midTermWeightScore;
+        }
         System.out.println("Weighted score: " + df.format(midTermWeightedScore) + "/" + midTermWeightScore);
 
         return midTermWeightedScore;
@@ -83,8 +88,8 @@ public class GradeStudent {
         if (finalTermTotalScore > 100) {
             finalTermTotalScore = 100;
         }
-        float finalTermWeightedScore = (((float)finalTermTotalScore / 100) * finalTermWeightScore);
-        System.out.println("Total points: " + (finalTermScoreEarned + finalTermShiftedAmount) + "/100");
+        float finalTermWeightedScore = (((float) finalTermTotalScore / 100) * finalTermWeightScore);
+        System.out.println("Total points: " + finalTermTotalScore + "/100");
         System.out.println("Weighted score: " + df.format(finalTermWeightedScore) + "/" + finalTermWeightScore);
 
         return finalTermWeightedScore;
