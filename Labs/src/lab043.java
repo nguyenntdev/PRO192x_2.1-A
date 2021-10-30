cf5843e8d47b3421e2b1c051dc3c3cea7c55a280
Nguyễn N. Thiện <thiennnfx10909@funix.edu.vn>
2021-10-30 09:17:39 +0700

    Added Lab 4.3 => 4.5

--- Labs/src/lab043.java
+++ Labs/src/lab043.java
@@ -1,2 +1,15 @@
-public class kab043 {
+public class lab043 {
+    public static void main(String[] args) {
+        int f0 = 1;
+        int f1 = 1;
+
+        System.out.print(f0 + " ");
+        System.out.print(f1 + " ");
+
+        for(int fk = 2; fk <= 144; fk = f1 + f0) {
+            System.out.print(fk + " ");
+            f0 = f1;
+            f1 = fk;
+        }
+    }
 }
---
+++ Labs/src/lab044.java
@@ -0,0 +1,7 @@
+public class lab044 {
+    public static void main(String[] args) {
+        for(int i = 0; i < 4; i++) {
+        System.out.println("*****");
+        }
+    }
+}
