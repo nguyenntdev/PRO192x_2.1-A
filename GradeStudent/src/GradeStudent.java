5240b60b03d91ca47b08c2d68cbf09a3095caa55
Nguyễn N. Thiện <kakangocthien109@gmail.com>
2021-10-31 17:11:08 -0700

    .

--- GradeStudent/src/GradeStudent.java
+++ GradeStudent/src/GradeStudent.java
@@ -1,4 +1,30 @@
+/**
+ * Assignment 2 - Nguyen Ngoc Thien - FX-10909 - 31/10/21
+ *
+ * GradeStudent - chương trình giúp tính điểm trung bình, GPA
+ * của từng bạn dựa trên dữ liệu đã cho. Chương trình có cấu
+ * trúc như sau:
+ * Gồm 5 hàm chính:
+ *  + midTerm():        hàm dùng để nhập và tính điểm GK
+ *  + finalTerm():      hàm dùng để nhập và tính điểm cuối kì
+ *  + homework():       hàm dùng để tính điểm Assignments,
+ *                      điểm chuyên cần,...v.v.
+ *  + report():         hàm dùng để tính và in điểm trung bình,
+ *                      tổng điểm, GPA,...v.v.
+ *
+ *  + main():           gọi các hàm trên và gán KQ return
+ *
+ * Trong đó có 3 hàm phụ để in thông tin, seperator...
+ *
+ * Global:
+ *  + DecimalFormat dùng để định dạng số TP ở dạng x.y
+ *  + Scanner dùng để thu input từ người dùng
+ */
+
 import java.util.*;
+
+import javax.swing.event.SwingPropertyChangeSupport;
+
 import java.text.*;
 
 public class GradeStudent {
@@ -19,210 +45,336 @@ public class GradeStudent {
         System.out.println("S  T  U  D  E  N  T   G  R  A  D  E");
     }
 
-    static void showIntro() {  // Hàm showIntro, in thông tin, showBrand cho biết chương trình làm gì
+    static void showIntro() {
         clearScreen();
         showSeperator();
         showBrand();
         showSeperator();
-        System.out.println("\nThis program reads exam/homework scores and reports your overall course grade.");
+        System.out.println("\nThis program reads exam/homework scores and reports your overall course grade.\n");
     }
 
-    static float midTerm() { // Hàm nhập và tính cột điểm giữa kì
-        DecimalFormat df = new DecimalFormat("#.#"); // Khai báo class DecimalFormat dùng để format số thập phân thành 1 chữ số thập phân
-        Scanner input = new Scanner(System.in); // Thêm lớp input
-        clearScreen();
-        showSeperator();
-        showBrand();
+    /** Hàm midTerm(): tính các cột điểm giữa kì
+     * - Đầu tiên sẽ định dạng STP rồi khai báo Scanner để thu input
+     * từ người dùng
+     * - Sau đó thực hiện hàm do - while để nhập điểm trọng số, trong
+     * đó nếu điểm trọng số > 100 thì báo lỗi và yêu cầu nhập lại (t/c
+     * nâng cao)
+     * - Tiếp theo nhập số điểm đã có trên thang điểm 100
+     * - Hỏi người dùng có tăng điểm không, nếu có thì nhập điểm tăng
+     * - Tính total score theo công thức là điểm đã có + điểm được
+     * tăng, nếu total score > 100 thì = 100, rồi in total score
+     * - Tiếp theo tính điểm tính theo trọng số, nếu > trọng số thì =
+     * trọng số
+     * - Cuối cùng trả điểm tính theo trọng số
+     */
+
+    static float midTerm() {
+
+        DecimalFormat df = new DecimalFormat("#.#");
+        Scanner input = new Scanner(System.in);
+
+
         showSeperator();
         System.out.println("M  I  D   T  E  R  M");
         showSeperator();
-        // Hàm do-while dùng để liên tiếp
-        // yêu cầu nhập trọng số nếu vượt
-        // limit (<= 100)
-        do { 
+
+        do {
 
             System.out.print("\nPlease enter weight score: ");
             midTermWeightScore = input.nextInt();
+
             if (midTermWeightScore > 100) {
                 System.out.print("Your weight score exceeded the limit. Please re-enter weight score.");
             }
 
         } while (midTermWeightScore > 100);
-        System.out.print("\nPlease enter score earned: "); // In ra dòng chữ yêu cầu người dùng nhập score
-        float midTermScoreEarned = input.nextFloat(); // Thu input từ người dùng
-        System.out.print("\nWere score shifted [y/n]? "); // Hỏi nếu điểm có được cộng không
-        String midTermShiftedChoice = input.next(); // Do dùng y/n (yes - no) nên phải dùng input string
 
-        int midTermShiftedAmount = 0; // khỏi tạo biến số điểm được thêm
-        if (midTermShiftedChoice.equalsIgnoreCase("y")) { // nếu có thì thực thi tiếp
-            System.out.print("\nPlease enter shifted amount: "); // in ra dòng chữ yêu cầu nhập số điểm được tăng
-            midTermShiftedAmount = input.nextInt(); // thu input từ người dùng
+        System.out.print("\nPlease enter score earned: ");
+        float midTermScoreEarned = input.nextFloat();
+
+        System.out.print("\nWere score shifted [y/n]? ");
+        String midTermShiftedChoice = input.next();
+
+        int midTermShiftedAmount = 0;
+
+        if (midTermShiftedChoice.equalsIgnoreCase("y")) {
+
+            System.out.print("\nPlease enter shifted amount: ");
+            midTermShiftedAmount = input.nextInt();
+
         }
+
         showSeperator();
-        float midTermTotalScore = midTermScoreEarned + midTermShiftedAmount; // tính total score, điểm có được + điểm cộng
-        if (midTermTotalScore > 100) { // nếu điểm có được > 100 thì = 100
+
+        float midTermTotalScore = midTermScoreEarned + midTermShiftedAmount;
+
+        if (midTermTotalScore > 100) {
+
             midTermTotalScore = 100;
+
         }
-        float midTermWeightedScore = (((float) midTermTotalScore) / 100) * midTermWeightScore; // tính điểm tính theo trọng số
-        System.out.println("\nTotal points: " + ((int) midTermTotalScore) + "/100"); // in total point
-        if (midTermWeightedScore > midTermWeightScore) { // nếu điểm tính theo trọng số > trọng số thì = trọng số
+
+        float midTermWeightedScore = (((float) midTermTotalScore) / 100) * midTermWeightScore;
+        System.out.println("\nTotal points: " + ((int) midTermTotalScore) + "/100");
+
+        if (midTermWeightedScore > midTermWeightScore) {
             midTermWeightedScore = midTermWeightScore;
         }
-        System.out.println("Weighted score: " + df.format(midTermWeightedScore) + "/" + midTermWeightScore + "\n"); // in điểm tính theo trọng số
 
-        return midTermWeightedScore;  // trả kq cho điểm tính theo trọng số
+        System.out.println("Weighted score: " + df.format(midTermWeightedScore) + "/" + midTermWeightScore + "\n");
+
+        return midTermWeightedScore;
+
     }
 
-    static float finalTerm() { // Hàm nhập và tính điểM cuối kì
-        DecimalFormat df = new DecimalFormat("#.#"); // Khai báo class DecimalFormat dùng để format số thập phân thành 1 chữ số thập phân
 
-        Scanner input = new Scanner(System.in); // thêm lớp input để lấy thông tin từ người dùng
+    /** Hàm finalTerm(): tính các cột điểm cuối kì
+     * - Đầu tiên sẽ định dạng STP rồi khai báo Scanner để thu input
+     * từ người dùng
+     * - Sau đó thực hiện hàm do - while để nhập điểm trọng số, trong
+     * đó nếu điểm trọng số cuối kì + giữa kỳ > 100 thì báo lỗi và
+     * yêu cầu nhập lại (t/c nâng cao)
+     * - Tiếp theo nhập số điểm đã có trên thang điểm 100
+     * - Hỏi người dùng có tăng điểm không, nếu có thì nhập điểm tăng
+     * - Tính total score theo công thức là điểm đã có + điểm được
+     * tăng, nếu total score > 100 thì = 100, rồi in total score
+     * - Tiếp theo tính điểm tính theo trọng số, nếu > trọng số thì =
+     * trọng số
+     * - Cuối cùng trả điểm tính theo trọng số
+     */
+
+    static float finalTerm() {
+        DecimalFormat df = new DecimalFormat("#.#");
+        Scanner input = new Scanner(System.in);
 
         showSeperator();
         System.out.println("F  I  N  A  L   T  E  R  M"); 
         showSeperator();
-        do { // loop vòng lặp này nếu weight score của midterm + finalterm > 100 đến khi < limit (100)
-            System.out.print("\nPlease enter weight score: \n"); // yêu cầu người dùng nhập weight score
 
-            finalTermWeightScore = input.nextInt(); // thu input weight score của người dùng
-            if ((finalTermWeightScore + midTermWeightScore) > 100) { // nếu > 100 yêu cầu nhập lại
+        do {
+            System.out.print("\nPlease enter weight score: ");
+            finalTermWeightScore = input.nextInt();
+
+            if ((finalTermWeightScore + midTermWeightScore) > 100) {
+
                 System.out.print("\nYour weight score exceeded the limit. Please re-enter weight score.");
+
             }
+
         } while ((finalTermWeightScore + midTermWeightScore) > 100);
 
-        System.out.print("\nPlease enter score earned: "); // yêu cầu nhập điểm có được
-        int finalTermScoreEarned = input.nextInt(); // lấy input từ người dùng
-        System.out.print("\nWere score shifted [y/n]? "); // hỏi điểm có thêm không
-        String finalTermShiftedChoice = input.next(); // lấy input kiểu string, y - n
+        System.out.print("\nPlease enter score earned: ");
+        int finalTermScoreEarned = input.nextInt();
 
-        int finalTermShiftedAmount = 0; // khởi tạo biến số điểm đã tăng = 0
-        if (finalTermShiftedChoice.equalsIgnoreCase("y")) { // nếu = y thì thực thi tiếp
-            System.out.print("\nPlease enter shifted amount: "); // y/c nhập số điểm đã tăng
-            finalTermShiftedAmount = input.nextInt(); // thu input
+        System.out.print("\nWere score shifted [y/n]? ");
+        String finalTermShiftedChoice = input.next();
+
+        int finalTermShiftedAmount = 0;
+
+        if (finalTermShiftedChoice.equalsIgnoreCase("y")) {
+
+            System.out.print("\nPlease enter shifted amount: ");
+            finalTermShiftedAmount = input.nextInt();
+
         }
+
         showSeperator();
-        float finalTermTotalScore = ((float) (finalTermShiftedAmount + finalTermScoreEarned)); // tính total score
-        if (finalTermTotalScore > 100) { // nếu total score > 100 thì = 100
+
+        float finalTermTotalScore = ((float) (finalTermShiftedAmount + finalTermScoreEarned));
+
+        if (finalTermTotalScore > 100) {
+
             finalTermTotalScore = 100;
+
         }
 
-        float finalTermWeightedScore = (((float) finalTermTotalScore) / 100) * finalTermWeightScore;// tính điểm tính theo trọng số
-        System.out.println("\nTotal points: " + ((int) finalTermTotalScore) + "/100"); // in điểm tổng
-        if (finalTermWeightedScore > finalTermWeightScore) { // nếu điểm tính theo trọng số > trọng số thì = trọng số
+        float finalTermWeightedScore = (((float) finalTermTotalScore) / 100) * finalTermWeightScore;
+        System.out.println("\nTotal points: " + ((int) finalTermTotalScore) + "/100");
+
+        if (finalTermWeightedScore > finalTermWeightScore) {
+
             finalTermWeightedScore = finalTermWeightScore;
-        } 
-        System.out.format("\nWeighted score: %.1f/%d\n\n", finalTermWeightedScore, finalTermWeightScore); //in điểm tính theo trọng số 
-        return finalTermWeightedScore; // trả về điểm tính theo trọng số
+
+        }
+
+        System.out.format("\nWeighted score: %.1f/%d\n\n", finalTermWeightedScore, finalTermWeightScore);
+
+        return finalTermWeightedScore;
     }
 
-    static float homework() { // hàm homework nhập và tính điểm homework, chuyên cần...
-        DecimalFormat df = new DecimalFormat("#.#"); // khởi tạo lớp format số TP 
 
-        Scanner input = new Scanner(System.in); // thêm 1 lớp input cho việc nhập DL
+    /** Hàm homework(): tính các cột điểm cuối kì
+     * - Đầu tiên sẽ định dạng STP rồi khai báo Scanner để thu input
+     * từ người dùng
+     * - Sau đó thực hiện hàm do - while để nhập điểm trọng số, trong
+     * đó nếu điểm trọng số cuối kì + giữa kỳ + homework > 100 thì báo
+     * lỗi và yêu cầu nhập lại (t/c nâng cao)
+     * - Nhập số bài KT (assignments) rồi nhập điểm và điểm cao nhất
+     * - Tiếp theo nhập số buổi đi học, tính điểm chuyên cần
+     * - Tính total score theo công thức là điểm đã có
+     * - Tiếp theo tính điểm tính theo trọng số, nếu > trọng số thì =
+     * trọng số
+     * - Sau đó in điểm tổng và điểm theo trọng số
+     * - Cuối cùng trả điểm tính theo trọng số
+     */
+
+
+    static float homework() {
+        DecimalFormat df = new DecimalFormat("#.#");
+        Scanner input = new Scanner(System.in);
 
         showSeperator();
         System.out.println("H  O  M  E  W  O  R  K");
         showSeperator();
-        int homeworkWeightScore = 0; // khởi tạo biến trọng số = 0
 
-        do { // nếu điểm trọng số của final, mid và homework cộng lại > 100 thì y/c nhập lại do đã trúng limit
+        int homeworkWeightScore = 0;
+
+        do {
 
             System.out.print("\nPlease enter weight score: ");
-
             homeworkWeightScore = input.nextInt();
 
             if ((finalTermWeightScore + homeworkWeightScore + midTermWeightScore) > 100) {
+
                 System.out.print("\nYour weight score exceeded the limit. Please re-enter weight score.\n");
+
             }
+
         } while ((finalTermWeightScore + homeworkWeightScore + midTermWeightScore) > 100);
-        System.out.print("\nPlease enter number of assignment: "); // nhập số bài tập
+
+        System.out.print("\nPlease enter number of assignment: ");
         int n = input.nextInt();
-        float[] scores = new float[n]; // khởi tạo mảng score, lưu asm score
-        float[] max = new float[n]; // khởi tạo mảng max, lưu điểm cao nhất
-        for (int i = 0; i < n; i++) { // tạo ra vòng lặp nhập điểm và max
+
+        float[] scores = new float[n];
+        float[] max = new float[n];
+
+        for (int i = 0; i < n; i++) {
+
             System.out.print("\nPlease enter the Assignment " + (i + 1) + " score and max: ");
             scores[i] = input.nextFloat(); 
             max[i] = input.nextFloat();
+
         }
 
-        int homeworkTotalScore = 0, homeworkMaxScore = 0; // khởi tạo biến asm total score và max score = 0 
+        int homeworkTotalScore = 0, homeworkMaxScore = 0;
 
-        for (int i = 0; i < n; i++) { // thêm số điểm total cho asm vào 1 biến, cộng lại
+        for (int i = 0; i < n; i++) {
+
             homeworkTotalScore += scores[i];
+
         }
 
-        for (int i = 0; i < n; i++) { // thêm số điểm max cho asm vào 1 biến, cộng lại
+        for (int i = 0; i < n; i++) {
+
             homeworkMaxScore += max[i];
+
         }
 
-        System.out.print("\nAmount of sections attended: "); // hỏi & y/c người dùng nhập số buổi dự
+        System.out.print("\nAmount of sections attended: ");
         int sectionsAttendedScore = (input.nextInt()) * 5;
+
         if (sectionsAttendedScore > 30) {
+
             sectionsAttendedScore = 30;
+
         }
 
-        System.out.print("\nSection point: " + sectionsAttendedScore + "/30\n"); // hiển thị section point
+        System.out.print("\nSection point: " + sectionsAttendedScore + "/30\n");
 
-        if (homeworkTotalScore > homeworkMaxScore) { // nếu total score của asm > max thì = max
+        if (homeworkTotalScore > homeworkMaxScore) {
+
             homeworkTotalScore = homeworkMaxScore;
+
         }
 
-        homeworkTotalScore = homeworkTotalScore + sectionsAttendedScore; // tính total score
-        homeworkMaxScore += 30; // thêm maxscore 30 điểm của sections attended max
+        homeworkTotalScore = homeworkTotalScore + sectionsAttendedScore;
+        homeworkMaxScore += 30;
 
-        if (homeworkTotalScore > 150) { // nếu asm total score > max (150) thì = 150
+        if (homeworkTotalScore > 150) {
+
             homeworkTotalScore = 150;
+
         }
 
-        if (homeworkMaxScore > 150) { // nếu max score > max 150 thì = 150 
+        if (homeworkMaxScore > 150) {
+
             homeworkMaxScore = 150;
+
         }
 
-        System.out.println("\nTotal points: " + homeworkTotalScore + "/" + homeworkMaxScore); // in total points
+        System.out.println("\nTotal points: " + homeworkTotalScore + "/" + homeworkMaxScore);
 
-        float homeworkWeightedScore = (((float) homeworkTotalScore / homeworkMaxScore) * homeworkWeightScore); // tinh điểm tính theo trọng số
+        float homeworkWeightedScore = (((float) homeworkTotalScore / homeworkMaxScore) * homeworkWeightScore);
 
-        System.out.println("\nWeighted score: "
-                + df.format((homeworkWeightedScore > homeworkWeightScore) ? homeworkWeightScore : homeworkWeightedScore)
-                + "/" + homeworkWeightScore + "\n\n"); // in điểm tính theo trọng số, nếu > trọng số thì = trọng số
+        System.out.println("\nWeighted score: " + df.format((homeworkWeightedScore > homeworkWeightScore) ? homeworkWeightScore : homeworkWeightedScore)
+                + "/" + homeworkWeightScore + "\n\n");
 
-        return homeworkWeightedScore; // trả về điểm theo trọng số
+        return homeworkWeightedScore;
     }
 
-    static void report(float midTermWeightedScore, float finalTermWeightedScore, float homeworkWeightedScore) { // hàm báo cáo
-        DecimalFormat df = new DecimalFormat("#.#");// khởi tạo lớp format
-        float grade = midTermWeightedScore + finalTermWeightedScore + homeworkWeightedScore;// tính điểm grade
-        System.out.println("\nYour grade is " + df.format(grade)); // in điểm grade
-        // Nếu grade >= 85 thì gpa = 3, >=75 thì gpa = 2, >= 60 thì gpa = 1, <60 thì gpa = 0
+    /** Hàm report(): dùng để in điểm trung bình, GPA...v.v
+     * - Đầu tiên lấy giá trị biến weighted score của các cột
+     * điểm.
+     * - Sau đó cộng lại và in grade score
+     * - Tiếp theo trả về GPA theo grade score
+     */
+
+    static void report(float midTermWeightedScore, float finalTermWeightedScore, float homeworkWeightedScore) {
+        DecimalFormat df = new DecimalFormat("#.#");
+
+        float grade = midTermWeightedScore + finalTermWeightedScore + homeworkWeightedScore;
+        System.out.println("\nYour grade is " + df.format(grade));
+
         float gpa = 0;
+
         if (grade >= 85) {
+
             gpa = 3;
             System.out.println("\nYour grade will be at least " + gpa);
+
         }
 
         else if (grade >= 75 && grade < 85) {
+
             gpa = 2;
             System.out.println("\nYour grade will be at least " + gpa);
+
         }
 
         if (grade >= 60 && grade < 75) {
+
             gpa = 1;
             System.out.println("\nYour grade will be at least " + gpa);
+
         }
 
         if (grade < 60) {
+
             gpa = 0;
             System.out.println("\nYour grade will be at least " + gpa);
+
         }
 
     }
 
+    /** Hàm main(): Gọi các hàm và gán giá trị được return vào
+     *              các biến, sau đó nhập vào hàm report
+     * */
 
     public static void main(String[] args) {
+
         showIntro();
+
         float midTermWeightedScore = midTerm(); //gọi & gán giá trị trả về vào biến
         float finalTermWeightedScore = finalTerm(); //gọi & gán giá trị trả về vào biến
         float homeworkWeightedScore = homework(); //gọi & gán giá trị trả về vào biến
         report(midTermWeightedScore, finalTermWeightedScore, homeworkWeightedScore); //gọi hàm report, nạp dl cần thiết
+
     }
 }
+
+/*
+* ==========================================
+* E N D    O F    F I L E
+* ==========================================
+*/
