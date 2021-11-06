66d07eb9385124020af5260de67afb22b37270ac
Nguyễn N. Thiện <kakangocthien109@gmail.com>
2021-11-05 21:09:39 -0700

    uPdAtE

--- HumanResources/src/HumanResources.java
+++ HumanResources/src/HumanResources.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+
 public class HumanResources {
     public static void clearScreen() {
         System.out.print("\033[H\033[2J");
@@ -13,18 +14,18 @@ public class HumanResources {
 
         do {
             System.out.println("==========================================================================\n" +
-                    "Human Resources - Assignment 2 - Nguyen Ngoc Thien\n" +
-                    "\n" +
-                    "Xin vui long chon chuc nang muon thuc hien [1 - 7]\n" +
-                    "\t1. Hien thi danh sach nhan vien \n" +
-                    "\t2. Hien thi cac bo phan \n" +
-                    "\t3. Hien thi cac nhan vien theo tung bo phan\n" +
-                    "\t4. Them nhan vien moi vao cong ty\n" +
-                    "\t5. Tim kiem thong tin nhan vien\n" +
-                    "\t6. Hien thi bang luong cua nhan vien trong cong ty\n" +
-                    "\t7. Hien thi bang luong cua nhan vien theo thu tu tang dan\n" +
-                    "\t8. Thoat\n" +
-                    "==========================================================================");
+                               "Human Resources - Assignment 2 - Nguyen Ngoc Thien\n" +
+                               "\n" +
+                               "Xin vui long chon chuc nang muon thuc hien [1 - 7]\n" +
+                               "\t1. Hien thi danh sach nhan vien \n" +
+                               "\t2. Hien thi cac bo phan \n" +
+                               "\t3. Hien thi cac nhan vien theo tung bo phan\n" +
+                               "\t4. Them nhan vien moi vao cong ty\n" +
+                               "\t5. Tim kiem thong tin nhan vien\n" +
+                               "\t6. Hien thi bang luong cua nhan vien trong cong ty\n" +
+                               "\t7. Hien thi bang luong cua nhan vien theo thu tu tang dan\n" +
+                               "\t8. Thoat\n" +
+                               "==========================================================================");
 
             choice = input.nextInt();
             if (!(choice > 0 && choice < 9)) {
@@ -36,7 +37,9 @@ public class HumanResources {
         return choice;
     }
 
+    public static void showEmployeeList() {
 
+    }
 
 
     public static void main(String[] args) {
